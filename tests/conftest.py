b12c041f925caabc87108f90813ad994c4dcd3a0
Jahnavi Gummalla <110349962+jahnavi-gummalla@users.noreply.github.com>
2026-05-01 00:38:16 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,21 @@ import pytest
 import requests
 
 BASE_URL = "https://jsonplaceholder.typicode.com"
+TIMEOUT = 10
+
 
 @pytest.fixture
 def base_url():
     return BASE_URL
+
 
 @pytest.fixture
 def session():
     s = requests.Session()
     s.headers.update({"Content-Type": "application/json"})
     return s
+
+
+@pytest.fixture
+def timeout():
+    return TIMEOUT
